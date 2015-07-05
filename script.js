3dde586bd5b14720587801d4daa24f302e2ab5ea
Jason Zhang <danceswithpixels91@gmail.com>
2015-07-04 17:05:26 -0700

    Initial commit of website
    Website to be hosted on Heroku.

---
+++ script.js
@@ -0,0 +1,9 @@
+function showdiv(id) {
+	var div = document.getElementById(id);
+	div.style.visibility = 'visible';
+}
+
+function hidediv(id) {
+	var div = document.getElementById(id);
+	div.style.visibility = 'hidden';
+}
